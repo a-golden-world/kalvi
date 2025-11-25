4cbd87c66939740ec3f03922c5132c44d4bfcd43
Kalvi by Shankar V <ava+kalvi@amadeusweb.world>
2025-11-25 21:05:39 +0530

    v1

--- cms.php
+++ cms.php
@@ -1,8 +1,19 @@
 <?php
+addStyle('styles');
+
 variables([
 	'sections-have-files' => true,
 	'link-to-section-home' => true,
 ]);
+
+if (nodeIs(SITEHOME))
+	variable('custom-heading', replaceHtml('<div class="bg-light rounded-5 row m-2 p-2 text-center">
+		<div class="col-sm-3" style="min-width: 150px;">
+			<a href="%url%" class="btn btn-outline-info bg-info"><img class="img-fluid" style="" src="%url%%safeName%-icon.png" /></a></div>
+		<div class="col align-middle"><h2>%siteName%</h2>'
+			. runAllMacros('%contact-snippet%') . '</a><br /></div></div>
+		<div class="m-2 p-2 bg-light text-center">'
+			. runAllMacros(replaceIfContained('%welcome-message%', 'welcome-message')) . '</a></div>'));
 
 function before_file() {
 	//do here so menu will still work
---
+++ data/snippets/contact.md
@@ -0,0 +1,6 @@
+[WhatsApp](%whatsapp%%enquiry%BTNWHATSAPP)
+[Email](mailto:%email%?subject=%enquiry%BTNEMAIL)
+[Call](tel:%phone%BTNPHONE)
+[Map](%address-url%BTNMAP)
+<hr class="w-60" />
+<small class="bg-body p-2">%address%</small>
